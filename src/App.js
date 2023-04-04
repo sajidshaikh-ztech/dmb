a31fe067e107de46665517275bda8a8f06a26777
Sajid Shaikh <sajid.shaikh@ey.com>
2023-04-04 15:36:44 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './Menu.css';
+import ReturningProduct from './component/ReturningProduct';
+import MainProduct from './component/MainProduct';
+import Header from './component/Header';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [menuItem, setMenuItem] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://9ea8a0e7-683e-4607-abbd-abaa1abf204a.mock.pstmn.io/menu?test=123');
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setMenuItem(jsonData.menuItems);
+      console.log(jsonData.menuItems);
+      setData(jsonData);
+    };
+
+    fetchData();
+  }, []);
+
+  /*useEffect(() => {
+    fetchData();
+    const intervalId = setInterval(fetchData, 10000); // Refresh data every 10 secs
+    return () => clearInterval(intervalId); // Cleanup function to clear interval on unmount
+  }, []);
+
+  const fetchData = () => {
+    fetch("https://r63km.wiremockapi.cloud/menu")
+      .then((response) => response.json())
+      .then((data) => {
+        setMenuItem(data.menuItems);
+        setData(data)
+      });
+  };*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <div id='panel1' className='panel1'>
+          <Header titles={data}/>
+          <div className='container'>
+          {menuItem.map((item, index) => {
+              if (item.menuStatus === 'OutOfStock') {
+                return <ReturningProduct product={item}/>;
+              } else {
+                return <MainProduct product={item}/>;
+              }
+            })}
+          </div>
+        </div>
     </div>
   );
 }
 
 export default App;
+
