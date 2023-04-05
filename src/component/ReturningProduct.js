3e8d6c8f8ea2db89bcd5a5b34435e09d651f09d6
Sajid Shaikh <sajid.shaikh@ey.com>
2023-04-05 09:17:27 -0700

    Removed $ sign

--- src/component/ReturningProduct.js
+++ src/component/ReturningProduct.js
@@ -5,7 +5,7 @@ function ReturningProduct(props) {
     <div className='out-of-stock-item'>
       <img className='item-image' src={props.product.alternateItem} alt={props.product.menuItemName} />
       <p className='item-name'>{props.product.menuItemName}</p>
-      <p className='item-price'>${props.product.menuItemPrice}</p>
+      <p className='item-price'>{props.product.menuItemPrice}</p>
       <p className='item-nutrition'>{props.product.menuItemNutrition}</p>
   </div>
   );
