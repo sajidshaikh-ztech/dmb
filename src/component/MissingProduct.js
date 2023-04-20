c2bacadad025308d6baea8093348424ffa0939ff
Sajid Shaikh <sajid.shaikh@ey.com>
2023-04-19 21:57:06 -0400

    Changes for Error condition

---
+++ src/component/MissingProduct.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './MissingProduct.css';
+
+function MissingProduct(props) {
+  return (
+    <div className='missing-item'>
+       <p>Please Define a Product</p>
+    </div>
+  );
+}
+
+export default MissingProduct;
--- src/component/QuadPromotion.js
+++ src/component/QuadPromotion.js
@@ -4,15 +4,18 @@ import ReturningProduct from './ReturningProduct';
 import MainProduct from './MainProduct';
 import ProductsDropDown from './ProductsDropDown';
 import PriceDropDown from './PriceDropDown';
+import MissingProduct from './MissingProduct';
 
 function QuadPromotion(props) {
     const [data, setData] = useState([]);
     const [menuItem, setMenuItem] = useState([]);
     const [optionValue, setOptionValue] = useState('breakfast');
     const [discount, setDiscount] = useState(0);
+    const [missingProduct, setmissingProduct] = useState('N');
 
     function handleOptionChange(newValue) {
       setOptionValue(newValue);
+      setmissingProduct('N');
       console.log('Option Value ' + optionValue);
     }
   
@@ -21,29 +24,25 @@ function QuadPromotion(props) {
       console.log('Price value ' + discount)
     }
 
+    const fetchData = async () => {
+      const response = await fetch('https://ximqh5cncg.execute-api.us-east-1.amazonaws.com/dev/menu?option=' + optionValue);
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setMenuItem(jsonData.menuItems);
+      console.log(jsonData.menuItems);
+      console.log('MenuItem length ' + jsonData.menuItems.length);
+      if (jsonData.menuItems.length < 4) {
+        setmissingProduct('Y');
+      }
+      console.log('Missing Products:' + missingProduct);
+      setData(jsonData);
+    };
+
     useEffect(() => {
-      const fetchData = async () => {
-        const response = await fetch('https://ximqh5cncg.execute-api.us-east-1.amazonaws.com/dev/menu?option=' + optionValue);
-        const jsonData = await response.json();
-        console.log(jsonData);
-        setMenuItem(jsonData.menuItems);
-        console.log(jsonData.menuItems);
-        setData(jsonData);
-      };
-  
       fetchData();
     }, [optionValue]); 
 
     useEffect(() => {
-      const fetchData = async () => {
-        const response = await fetch('https://ximqh5cncg.execute-api.us-east-1.amazonaws.com/dev/menu?option=' + optionValue);
-        const jsonData = await response.json();
-        console.log(jsonData);
-        setMenuItem(jsonData.menuItems);
-        console.log(jsonData.menuItems);
-        setData(jsonData);
-      };
-  
       fetchData();
     }, []);
 
@@ -52,14 +51,21 @@ function QuadPromotion(props) {
       <div id='panel1' className= 'panel1'>
           <Header titles={data}/>
           <div className='container'>
-          {menuItem.map((item, index) => {
+          {
+            menuItem.map((item, index, menuItem) => {
               if (item.menuStatus === 'OutOfStock') {
                 return <ReturningProduct product={item}/>;
               } else {
                 return <MainProduct product={item} discount={discount}/>;
-              }
-            })}
+              } 
+              
+            })
+          }
+           
+          { missingProduct == 'Y' ? <MissingProduct/> : <div></div>}
+           
           </div>
+         
           <div className='container'>
             <ProductsDropDown option={optionValue} onChange={handleOptionChange} />
             <PriceDropDown discount={discount} onChange={applyDiscount} />
